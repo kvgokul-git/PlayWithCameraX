c3e253f1fe3ab90eeda956f094a87f7bfa2368fc
kvgokul-git <kvgokul.git@gmail.com>
2020-09-24 14:19:55 +0100

    Initial Commit

--- app/src/main/java/com/gklabs/playwithcamerax/MainActivity.kt
+++ app/src/main/java/com/gklabs/playwithcamerax/MainActivity.kt
@@ -143,7 +143,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val TAG = "CameraXBasic"
+        private const val TAG = "PlayWithCameraX"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
